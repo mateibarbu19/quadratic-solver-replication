38092f0d5030b7f79ec61619af450a3a1cf6b280
Matei Barbu <matei.barbu1905@stud.acs.upb.ro>
2023-05-29 13:37:16 +0300

    unittests

---
+++ tests/test3.m
@@ -0,0 +1,23 @@
+x0 = [];
+
+H = [
+    1;
+];
+
+q = [
+    -7;
+];
+
+A = [
+    3;
+];
+
+b = [
+    6;
+];
+
+our = demoQP(x0, H, q, A, b, [], [])
+
+correct = qp(x0, H, q, A, b)
+
+assert(our, correct, 1e-12);
---
+++ tests/test6.m
@@ -0,0 +1,26 @@
+x0 = [];
+
+H = [
+     1 -1;
+    -1  2;
+];
+
+q = [
+    -2;
+    -6;
+];
+
+
+A = [
+    1 1;
+];
+
+b = [
+    0;
+];
+
+our = demoQP(x0, H, q, A, b, [], [])
+
+correct = qp(x0, H, q, A, b)
+
+assert(our, correct, 1e-12);
